16d7b5f0fd2ab64614235853643b149d48fa2023
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-12-16 13:15:50 +0545

    movie model registered to admin site and customized

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'language', 'status', 'year_of_production', 'views_count')
+    list_display_links = ('id', 'title')
+    list_per_page = 10
+    search_fields = ('title', 'description')
+    list_filter = ('category', 'language', 'status', 'year_of_production', 'views_count')
+
+
+admin.site.register(Movie, MovieAdmin)
