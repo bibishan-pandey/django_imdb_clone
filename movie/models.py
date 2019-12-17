c29ccddbdd654b845905f58a7044d30e279310a8
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-12-17 17:39:15 +0545

    adding slug

--- movie/models.py
+++ movie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 CATEGORY_CHOICES = (
@@ -33,7 +34,14 @@ class Movie(models.Model):
     cast = models.CharField(max_length=100)
     year_of_production = models.DateField()
     views_count = models.BigIntegerField(default=0)
+
+    slug = models.SlugField(blank=True, null=True)
     # tags
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Movie, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- movie/urls.py
+++ movie/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', views.MovieList.as_view(), name='home'),
     # path('<int:pk>/', views.MovieDetail.as_view(), name='detail'),
     # path('category/<str:category>/', views.MovieCategory.as_view(), name='category'),
-    path(r'^(?P<int:pk>\d+)/', views.MovieDetail.as_view(), name='detail'),
+    path(r'^(?P<slug:slug>\d+)/', views.MovieDetail.as_view(), name='detail'),
     path(r'^genre category/(?P<category>\D+)/', views.GenreCategory.as_view(), name='genre category'),
     path(r'^language category/(?P<category>\D+)/', views.LanguageCategory.as_view(), name='language category'),
     path(r'^search/', views.MovieSearch.as_view(), name='search'),
-    path('year/<int:year>/', views.MovieYear.as_view(), name='year'),
+    path(r'^year/(?P<int:year>\d+)/', views.MovieYear.as_view(), name='year'),
 ]
